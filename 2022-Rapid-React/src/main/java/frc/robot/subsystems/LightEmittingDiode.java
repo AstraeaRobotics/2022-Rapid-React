2f6e5c4ed23202437362f640743ea679e73539d8
redt1979 <advik360@gmail.com>
2022-07-19 10:27:08 -0400

    Created LightEmittingDiode

---
+++ 2022-Rapid-React/src/main/java/frc/robot/subsystems/LightEmittingDiode.java
@@ -0,0 +1,24 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+
+
+
+public class LightEmittingDiode extends SubsystemBase {
+  /** Creates a new LightEmittingDiode. */
+  public LightEmittingDiode() {
+    AddressableLED LightEmittingDiode = new AddressableLED(3); //this port cannot be changed
+    
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
