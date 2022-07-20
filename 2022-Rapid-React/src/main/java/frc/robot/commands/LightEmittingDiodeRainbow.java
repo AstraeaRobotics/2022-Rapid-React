877a6e1de0d207849eaf2d5c3a17d4b62e72c229
redt1979 <advik360@gmail.com>
2022-07-20 11:13:47 -0400

    commandss

--- 2022-Rapid-React/src/main/java/frc/robot/commands/LightEmittingDiodeRainbow.java
+++ 2022-Rapid-React/src/main/java/frc/robot/commands/LightEmittingDiodeRainbow.java
@@ -10,19 +10,26 @@ public class LightEmittingDiodeRainbow extends CommandBase {
   /** Creates a new LightEmittingDiodeRainbow. */
   public LightEmittingDiodeRainbow() {
     // Use addRequirements() here to declare subsystem dependencies.
+    
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+
+  }
 
   // Returns true when the command should end.
   @Override
