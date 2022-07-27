a420d5ceecd2e2e530169f0d9f863a70dfb6b1b1
2AK7 <108776543+2AK7@users.noreply.github.com>
2022-07-27 10:36:11 -0400

    Cleaned RobotContainer

--- 2022-Rapid-React/src/main/java/frc/robot/RobotContainer.java
+++ 2022-Rapid-React/src/main/java/frc/robot/RobotContainer.java
@@ -55,48 +55,6 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // RobotContainer.m_driveSubsystem.resetOdometry(Traj.createNewTrajectoryFromJSON("OneBall-1").getInitialPose());
-
-    // return new SequentialCommandGroup(
-    //     Ramsete.createRamseteCommand(Traj.createNewTrajectoryFromJSON("OneBall-1"), m_driveSubsystem, true));
-
-    // RobotContainer.m_driveSubsystem.resetOdometry(Traj.createNewTrajectoryFromJSON("TwoBall").getInitialPose());
-    // return new SequentialCommandGroup(
-    //   Ramsete.createRamseteCommand(Traj.createNewTrajectoryFromJSON("TwoBall"), m_driveSubsystem, true)
-    // );
-
-    // RobotContainer.m_driveSubsystem.resetOdometry(Traj.createNewTrajectoryFromJSON("CirclePath").getInitialPose());
-    // return new SequentialCommandGroup(
-    //   Ramsete.createRamseteCommand(Traj.createNewTrajectoryFromJSON("CirclePath"), m_driveSubsystem, true));
-
-    /* //ThreeBall
-    RobotContainer.m_driveSubsystem.resetOdometry(Traj.createNewTrajectoryFromJSON("ThreeBall-1").getInitialPose());
-    return new SequentialCommandGroup(
-      Ramsete.createRamseteCommand(Traj.createNewTrajectoryFromJSON("ThreeBall-1"), m_driveSubsystem, true)
-      //Intake
-      //Standing Turn
-      //Shoot
-      Ramsete.createRamseteCommand(Traj.createNewTrajectoryFromJSON("ThreeBall-2"), m_driveSubsystem, true)
-      //Intake
-      //Standing Turn
-      Ramsete.createRamseteCommand(Traj.createNewTrajectoryFromJSON("ThreeBall-3"), m_driveSubsystem, true)
-      //Shoot
-    );
-    */
-
-    // RobotContainer.m_driveSubsystem.resetOdometry(Traj.createNewTrajectoryFromJSON("TwoBall-1").getInitialPose());
-    // return new SequentialCommandGroup(
-    //   Ramsete.createRamseteCommand(Traj.createNewTrajectoryFromJSON("TwoBall-1"), m_driveSubsystem, true),
-    //   //Intake
-    //   Ramsete.createRamseteCommand(Traj.createNewTrajectoryFromJSON("TwoBall-2"), m_driveSubsystem, true)
-    //   //Shoot
-    //   );
-
-    // RobotContainer.m_driveSubsystem.resetOdometry(Traj.createNewTrajectoryFromJSON("StraightPath").getInitialPose());
-    //   return new SequentialCommandGroup(
-    //     Ramsete.createRamseteCommand(Traj.createNewTrajectoryFromJSON("StraightPath"), m_driveSubsystem, true));
-    // // return new DriveWait(m_driveSubsystem);
-
     return new StraightPath(m_driveSubsystem);
   }
 }
